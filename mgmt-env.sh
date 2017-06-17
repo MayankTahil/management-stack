0d61beeb7a926997230bad1e00b541c59aa7ea6c
Mayank Tahilramani <Mayank@Mandelbrot.local>
2017-06-16 17:11:51 -0700

    fixed a few things

--- mgmt-env.sh
+++ mgmt-env.sh
@@ -1,31 +1,35 @@
 # Adjustable Environment Variables. Change the following variable to suit your needs
 
 export PASS='Password01'
-export NET='172.16.20'
-export GATEWAY='172.16.10.11'
-export DOMAIN='citrix.lab'
+export NET='172.16.20'  #First three octets of your LAN network
+export GATEWAY='172.16.10.11' # Gateway to internet
+export DOMAIN='citrix.lab' # Desired domain for LDAP 
+export DATA='/mgmt_data' # Directory to keep all persistent data
 
 # Static Recomended Environment Variables
 
 ## Default host network interface to use to share same L2 network with containers. 
-export ETH='eth0'
+export ETH='eth0' # Interface for macvland driver to create Docker Network with
+
+#### Static Variables to store persistent service data.
 
 ## LDAP Variables
-export LDAP_DB='/mgmt_data/ldap/db'
-export LDAP_CONF='/mgmt_data/ldap/config'
+export LDAP_DB='$DATA/ldap/db'
+export LDAP_CONF='$DATA/ldap/config'
 
 ## Bind_DNS Variables
-export DNS_DATA='/mgmt_data/dns'
+export DNS_DATA='$DATA/dns'
 
 ## ISC_DHCP Configfile
-export DHCP_CONF='/mgmt_data/dhcp'
+#export DHCP_CONF='$DATA/dhcp'
+
 ## Guacamole Variables
-export GUAC_CONF='/mgmt_data/guac/conf'
-export GUAC_DATA='/mgmt_data/guac'
+export GUAC_CONF='$DATA/guac/conf'
+export GUAC_DATA='$DATA/guac'
 
 ## XenOrchestra Variables
-export XO_DATA='/mgmt_data/xo/conf'
-export REDIS_DATA='/mgmt_data/xo/redis'
+export XO_DATA='$DATA/xo/conf'
+export REDIS_DATA='$DATA/xo/redis'
 
 ## IPAM Variables
-export IPAM_SQL='/mgmt_data/ipam/sql'
+export IPAM_SQL='$DATA/ipam/sql'
